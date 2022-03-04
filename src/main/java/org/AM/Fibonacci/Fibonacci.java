4921b0923a1a0e35db88f76fa5008a96360532d6
AlexMedinaUma <alexmedinaastorga@uma.es>
2022-03-04 11:51:56 +0100

    Commit with zero and one cases

--- src/main/java/org/AM/Fibonacci/Fibonacci.java
+++ src/main/java/org/AM/Fibonacci/Fibonacci.java
@@ -1,4 +1,23 @@
 package org.AM.Fibonacci;
 
+/**
+ * Class providing a method that computes Fibonacci on a natural number
+ *
+ * @author Alejandro Medina Astorga
+ */
+
 public class Fibonacci {
+    public int compute(int value) {
+        int result = 0;
+        if(value == 0){
+            result = 0;
+        }
+        if(value == 1){
+            result = 1;
+        }
+
+
+
+        return result;
+    }
 }
--- src/test/java/org/AM/Fibonacci/FibonacciTest.java
+++ src/test/java/org/AM/Fibonacci/FibonacciTest.java
@@ -1,4 +1,33 @@
 package org.AM.Fibonacci;
 
-public class FibonacciTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test cases:
+ * Fibonacci 0 -> 0
+ * Fibonacci 1 -> 1
+ * Fibonacci n -> Fibonacci(n - 1) + Fibonacci(n-2)
+ * Fibonacci NegativeNumber -> ERROR
+ */
+class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @Test
+    public void testComputeReturnZeroIfTheNumberIsZero() { // This method retun 0 if the computed value is 0
+        fibonacci = new Fibonacci();
+        int expectedValue = 0;
+        int obtainedValue = fibonacci.compute(0);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
+    @Test
+    public void testComputeReturnOneIfTheNumberIsOne() { // This method retun 1 if the computed value is 1
+        fibonacci = new Fibonacci();
+        int expectedValue = 1;
+        int obtainedValue = fibonacci.compute(1);
+
+        assertEquals(expectedValue,obtainedValue);
+    }
 }
